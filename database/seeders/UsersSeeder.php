155db726b4b8b32c602b43ddc32bdb70e2453b22
LeonardoScherer <leoblacena11@gmail.com>
2023-04-10 13:56:03 -0300

    Seeder usuário admin

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('@Generic123'),
+            'is_admin' => 1,
+            'email_verified_at' => now(),
+        ]);
+    }
+}
