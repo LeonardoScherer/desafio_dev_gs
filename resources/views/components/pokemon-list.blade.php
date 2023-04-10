e5a9b9d188f2eb229a1228c1269508b183277811
LeonardoScherer <leoblacena11@gmail.com>
2023-04-10 13:55:34 -0300

    removendo onclick

--- resources/views/components/pokemon-list.blade.php
+++ resources/views/components/pokemon-list.blade.php
@@ -3,7 +3,7 @@
 
         <div style="background-color: rgb(30 41 59);" class="px-4 py-5 bg-sky-500 sm:p-6 shadow sm:rounded-md">
 
-            <img src="{{ $pokemon['pokemon_gif'] }}" alt="{{ $pokemon['name'] }}" style="width:48px;height:48px; float: right; margin-left: auto;" onclick='Livewire.emit("openModal", "pokemon-view", @json(["pokemon" => $pokemon]))'>
+            <img src="{{ $pokemon['pokemon_gif'] }}" alt="{{ $pokemon['name'] }}" style="width:48px;height:48px; float: right; margin-left: auto;">
 
             <h1 class="block text-white font-bold ml-3"><span class="text-slate-600">{{__('messages.Name')}}:</span> {{ $pokemon['name']}} </h1>
             <h3 class="text-white ml-3 font-bold"><span class="text-slate-600">{{__('messages.Location area')}}:</span> {{ $pokemon['location_area_encounters'] }}</h3>
